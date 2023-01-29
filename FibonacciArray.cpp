534820c9102eab9a26bf6fa9c1b5298197523c74
Mehmet Emin Küçükkurt <123323769+Kucukkurt1@users.noreply.github.com>
2023-01-29 03:00:50 +0300

    Fibonacci Array for C++

---
+++ FibonacciArray.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main () {
+	
+int num1=0;
+int num2=1;
+int i;
+
+for(i=0;i<=12;i++){
+
+ int result=num2;
+ 
+ num2+=num1; 
+ 
+ num1=result;
+ 
+ printf("%d\n",num2);
+}
+	
+
+
+
+	
+}
+	
+	
+	
+	
+	
+
